95c90112029f2fe1d7fbbd4941a6d14de730d149
Mehmet <mhmt_ylmz_@outlook.com>
2024-12-16 22:43:44 +0300

    dosya duzenlemesi

---
+++ Body System/Body System/Tools/Storage/Storage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Body_System.Tools.Storage
+{
+    public class Storage
+    {
+        [Required]
+        private StorageUnit _head;
+        List(int boyut)
+        {
+
+        }
+    }
+}
---
+++ Body System/Body System/Tools/Storage/StorageUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Body_System.Tools.Storage
+{
+    internal class StorageUnit
+    {
+        public int Data;
+        public StorageUnit? Next;
+        public int empty;
+        public StorageUnit(int data)
+        {
+            Data = data;
+            Next = null;
+            empty = 1;
+        }
+        public StorageUnit()
+        {
+            Data = 0;
+            Next = null;
+        }
+    }
+}
